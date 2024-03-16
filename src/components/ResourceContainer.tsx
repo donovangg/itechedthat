2199b166aafd72cb737b4582a609057ac76af417
Donovan Gomez <donovanjuniogomez@gmail.com>
2024-03-16 01:47:29 -0700

    Add filter functionality

--- src/components/ResourceContainer.tsx
+++ src/components/ResourceContainer.tsx
@@ -12,15 +12,71 @@ interface ResourceContainerProps {
 }
 
 const ResourceContainer = ({ resources }: ResourceContainerProps) => {
-  const [filtered, setFiltered] = useState("All");
+  const [isLoading, setIsLoading] = useState(true);
+  const [filteredResources, setFilteredResources] = useState<ResourceProps[]>(
+    []
+  );
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  // loading useEffect
+  useEffect(() => {
+    // Set isLoading to true for initial render of resources
+    setIsLoading(!resources);
+    // Initialize filteredResources with all resources
+    setFilteredResources(resources);
+  }, [resources]);
+
+  // filter useEffect
+  useEffect(() => {
+    filterResources(selectedCategory);
+  }, [selectedCategory]);
+
+  const filterResources = (category: string) => {
+    if (category === "All") {
+      setFilteredResources(resources);
+    } else {
+      const filtered = resources.filter(
+        (resource) => resource.category === category
+      );
+      setFilteredResources(filtered);
+    }
+  };
+
+  const handleCategoryChange = (category: string) => {
+    setSelectedCategory(category);
+  };
+
   return (
     <div>
       <h1>The resource container</h1>
-      {resources.map((res) => (
-        <article>
-          <h2 className="text-white">{res.title}</h2>
-        </article>
-      ))}
+      <div>
+        {/* Filter buttons */}
+        <button onClick={() => handleCategoryChange("All")}>All</button>
+        {/* Render filter buttons based on unique categories */}
+        {Array.from(new Set(resources.map((res) => res.category))).map(
+          (category) => (
+            <button
+              key={category}
+              onClick={() => handleCategoryChange(category)}
+            >
+              {category}
+            </button>
+          )
+        )}
+      </div>
+      {isLoading ? (
+        // Conditional rendering based on isLoading state
+        <p>Loading...</p>
+      ) : (
+        <>
+          {filteredResources.map((res) => (
+            <article key={res.id}>
+              {" "}
+              <h2 className="text-white">{res.title}</h2>
+            </article>
+          ))}
+        </>
+      )}
     </div>
   );
 };
