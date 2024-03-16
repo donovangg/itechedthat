57c4647074ebb092af3d78cf46973662496f20b4
Donovan Gomez <donovanjuniogomez@gmail.com>
2024-03-16 01:19:14 -0700

    update db schema

--- db/config.ts
+++ db/config.ts
@@ -2,11 +2,13 @@ import { defineDb, defineTable, column } from "astro:db";
 
 const Resource = defineTable({
   columns: {
+    id: column.number({
+      primaryKey: true,
+    }),
     link: column.text(),
     category: column.text(),
     title: column.text(),
     imgSrc: column.text(),
-    id: column.number(),
   },
 });
 
