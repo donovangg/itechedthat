b1f27e1ad16ca7840c9d595266aef7ced0828d99
Donovan Gomez <donovanjuniogomez@gmail.com>
2024-03-16 21:39:43 -0700

    update buttons and button container

--- src/components/ResourceContainer.tsx
+++ src/components/ResourceContainer.tsx
@@ -49,16 +49,30 @@ const ResourceContainer = ({ resources }: ResourceContainerProps) => {
   return (
     <div>
       <h1>The resource container</h1>
-      <div>
+      <div className="border-2 max-w-screen-2xl mx-auto flex gap-2">
         {/* Filter buttons */}
-        <button onClick={() => handleCategoryChange("All")}>All</button>
+        <button
+          className={`group relative inline-flex h-12 items-center justify-center overflow-hidden rounded-lg bg-zinc-900 ${
+            selectedCategory == "All" ? "bg-white text-zinc-950" : ""
+          } px-6 font-medium text-neutral-200`}
+          onClick={() => handleCategoryChange("All")}
+        >
+          All
+        </button>
+
         {/* Render filter buttons based on unique categories */}
         {Array.from(new Set(resources.map((res) => res.category))).map(
           (category) => (
             <button
+              className={`group relative inline-flex h-12 items-center justify-center overflow-hidden rounded-lg bg-zinc-900 ${
+                category == selectedCategory ? "bg-white text-zinc-950" : ""
+              } px-6 font-medium text-neutral-200`}
               key={category}
               onClick={() => handleCategoryChange(category)}
             >
+              <div className="absolute inset-0 flex h-full w-full justify-center [transform:skew(-12deg)_translateX(-100%)] group-hover:duration-1000 group-hover:[transform:skew(-12deg)_translateX(100%)]">
+                <div className="relative h-full w-8 bg-white/20"></div>
+              </div>
               {category}
             </button>
           )
