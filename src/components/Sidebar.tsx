eed0e5787d9a1ea0b0c8bd1a3622a37deef5a91a
Donovan Gomez <donovanjuniogomez@gmail.com>
2024-03-20 02:22:06 -0700

    adjust layout component so hero never changes only thing that changes is below the hero

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <aside className="fixed bottom-0 w-full bg-orange-600 overflow-y-scroll max-h-full">
+      Sidebar
+    </aside>
+  );
+};
+
+export default Sidebar;
