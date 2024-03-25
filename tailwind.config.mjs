5d28e1c32894a06f3e2ad11a8a5fbf8a6bb96ca5
Donovan Gomez <donovanjuniogomez@gmail.com>
2024-03-25 00:20:44 -0700

    Add custom headers

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        airdasher: "url('/assets/mr-dolphin.webp')",
+        capcom: "url('/assets/capcom.webp')",
+        fundamentals: "url('/assets/footsies.gif')",
+        bread: "url('/assets/akiha.webp')",
+        "3d": "url('/assets/3d.webp')",
+        submit: "url(/assets/mentos.webp)",
+      },
+    },
+  },
+  plugins: [],
+};
