c60254903a4adca978b0e45d242cef06a949dccf
Donovan Gomez <donovanjuniogomez@gmail.com>
2024-04-04 19:10:21 -0700

    add sticky nav, add hover effect on Submit button

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -6,7 +6,7 @@ function Dropdown() {
     const activeCategory = window.location.href;
     const category = activeCategory.split("/")[3];
   return (
-    <div className="text-right z-50">
+    <div className=" border-2 border-orange-600 z-50">
       <Menu as="div" className="relative inline-block text-left">
         <div>
           <Menu.Button className="inline-flex w-full justify-center rounded-md px-4 py-2 text-sm font-medium text-white hover:text-violet-500 transition duration-75 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
@@ -27,14 +27,14 @@ function Dropdown() {
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
         >
-          <Menu.Items className="absolute left-0 mt-2 z-50 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <Menu.Items className="absolute left-0 mt-[-15.55rem] z-50 w-96 origin-bottom-right  rounded-md bg-zinc-800 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
             <div className="px-1 py-1 ">
               <Menu.Item>
                 {({ active }) => (
                   <a href="/fundamentals">
                     <button
                       className={`${
-                        active ? "bg-violet-500 text-white" : "text-gray-900"
+                        active ? "bg-violet-500 text-white" : "text-neutral-200"
                       } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
                     >
                       Fundamentals
@@ -49,7 +49,7 @@ function Dropdown() {
                   <a href="/capcom">
                     <button
                       className={`${
-                        active ? "bg-violet-500 text-white" : "text-gray-900"
+                        active ? "bg-violet-500 text-white" : "text-neutral-200"
                       } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
                     >
                       Capcom
@@ -62,7 +62,7 @@ function Dropdown() {
                   <a href="/airdashers">
                     <button
                       className={`${
-                        active ? "bg-violet-500 text-white" : "text-gray-900"
+                        active ? "bg-violet-500 text-white" : "text-neutral-200"
                       } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
                     >
                       Airdashers
@@ -75,7 +75,7 @@ function Dropdown() {
                   <a href="/3d">
                     <button
                       className={`${
-                        active ? "bg-violet-500 text-white" : "text-gray-900"
+                        active ? "bg-violet-500 text-white" : "text-neutral-200"
                       } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
                     >
                       3D
@@ -90,7 +90,7 @@ function Dropdown() {
                   <a href="/french-bread">
                     <button
                       className={`${
-                        active ? "bg-violet-500 text-white" : "text-gray-900"
+                        active ? "bg-violet-500 text-white" : "text-neutral-200"
                       } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
                     >
                       French Bread
