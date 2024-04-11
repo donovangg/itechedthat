626f06beb67e240cd4b8056a56ddcda74655402f
Donovan Gomez <donovanjuniogomez@gmail.com>
2024-04-10 19:53:34 -0700

    add conditional code for Category when dropdown is in Home

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -11,7 +11,7 @@ function Dropdown() {
         <div>
           <Menu.Button className="inline-flex w-full justify-center rounded-md px-4 py-2 text-sm font-medium text-white hover:text-violet-500 transition duration-75 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
             <div className="flex items-center gap-2">
-              <span className="text-lg">{category}</span>
+              <span className="text-lg">{category === "" ? "Home": category}</span>
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-down" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"/>
 </svg>
